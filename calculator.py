f0c58a98aa99d586a8b863e052199db6567faf7c
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 15:47:40 -0500

    Made it into a loop

--- calculator.py
+++ calculator.py
@@ -5,23 +5,24 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
-input_string = input("Enter your problem: ")
-tokens = input_string.split(" ")
-if tokens[0] == "q":
-    quit
-elif tokens[0] == '+':
-    print(add(float(tokens[1]), float(tokens[2])))
-elif tokens[0] == '-':
-    print(subtract(float(tokens[1]), float(tokens[2])))
-elif tokens[0] == '*':
-    print(multiply(float(tokens[1]),float(tokens[2])))
-elif tokens[0] == '/':
-    print(divide(float(tokens[1]),float(tokens[2]))) 
-elif tokens[0] == 'square':
-    print(square(float(tokens[1])))
-elif tokens[0] =='cube':
-    print(cube(float(tokens[1])))
-elif tokens[0] == 'pow':
-    print(power(float(tokens[1]), float(tokens[2])))
-elif tokens[0] == 'mod':
-    print(mod(float(tokens[1]), float(tokens[2])))
+while True:
+    input_string = input("Enter your problem: ")
+    tokens = input_string.split(" ")
+    if tokens[0] == "q":
+        quit
+    elif tokens[0] == '+':
+        print(add(float(tokens[1]), float(tokens[2])))
+    elif tokens[0] == '-':
+        print(subtract(float(tokens[1]), float(tokens[2])))
+    elif tokens[0] == '*':
+        print(multiply(float(tokens[1]),float(tokens[2])))
+    elif tokens[0] == '/':
+        print(divide(float(tokens[1]),float(tokens[2]))) 
+    elif tokens[0] == 'square':
+        print(square(float(tokens[1])))
+    elif tokens[0] =='cube':
+        print(cube(float(tokens[1])))
+    elif tokens[0] == 'pow':
+        print(power(float(tokens[1]), float(tokens[2])))
+    elif tokens[0] == 'mod':
+        print(mod(float(tokens[1]), float(tokens[2])))
