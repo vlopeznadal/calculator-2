379a9978b53b179c152c0ca88b05e2a339731790
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 15:36:24 -0500

    finished cube part of Repl

--- calculator.py
+++ calculator.py
@@ -19,3 +19,5 @@ elif tokens[0] == '/':
     print(divide(float(tokens[1]),float(tokens[2]))) 
 elif tokens[0] == 'square':
     print(square(float(tokens[1])))
+elif tokens[0] =='cube':
+    print (cube(float(tokens[1])))
