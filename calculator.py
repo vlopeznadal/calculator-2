6ae8a755b4798efd6dedabbdb4c6192af8212841
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 15:24:53 -0500

    completed subtraction part of repl

--- calculator.py
+++ calculator.py
@@ -9,5 +9,7 @@ input_string = input("Enter your problem: ")
 tokens = input_string.split(" ")
 if tokens[0] == "q":
     quit
-elif tokens[0] == 'add':
+elif tokens[0] == '+':
     print(add(float(tokens[1]), float(tokens[2])))
+elif tokens[0] == '-':
+    print(subtract(float(tokens[1]), float(tokens[2])))    
