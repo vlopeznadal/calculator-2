c2d213b2f5984e27201b4886ad832a376046eddd
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 15:18:10 -0500

    Completed add part of REPL

--- calculator.py
+++ calculator.py
@@ -5,3 +5,9 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+input_string = input("Enter your problem: ")
+tokens = input_string.split(" ")
+if tokens[0] == "q":
+    quit
+elif tokens[0] == 'add':
+    print(add(float(tokens[1]), float(tokens[2])))
