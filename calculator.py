866d5ba870e80bc51a7b553c20e34509f864ee75
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 16:30:16 -0500

    Fixed decimal places to 2

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ while True:
     input_string = input("Enter your problem: ")
     tokens = input_string.split(" ")
     if tokens[0] == "q":
-        quit
+        break
     elif tokens[0] == '+':
         print(add(float(tokens[1]), float(tokens[2])))
     elif tokens[0] == '-':
