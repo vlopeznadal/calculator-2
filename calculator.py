e6fd4af25684a84adae67a22c0acdfed42a6a770
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 16:35:19 -0500

    Changed program from prefix notation to infix notation

--- calculator.py
+++ calculator.py
@@ -10,19 +10,19 @@ while True:
     tokens = input_string.split(" ")
     if tokens[0] == "q":
         break
-    elif tokens[0] == '+':
-        print(add(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '-':
-        print(subtract(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '*':
-        print(multiply(float(tokens[1]),float(tokens[2])))
-    elif tokens[0] == '/':
-        print(divide(float(tokens[1]),float(tokens[2]))) 
-    elif tokens[0] == 'square':
-        print(square(float(tokens[1])))
-    elif tokens[0] =='cube':
-        print(cube(float(tokens[1])))
-    elif tokens[0] == 'pow':
-        print(power(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == 'mod':
-        print(mod(float(tokens[1]), float(tokens[2])))
+    elif tokens[1] == '+':
+        print(add(float(tokens[0]), float(tokens[2])))
+    elif tokens[1] == '-':
+        print(subtract(float(tokens[0]), float(tokens[2])))
+    elif tokens[1] == '*':
+        print(multiply(float(tokens[0]),float(tokens[2])))
+    elif tokens[1] == '/':
+        print(divide(float(tokens[0]),float(tokens[2]))) 
+    elif tokens[1] == 'square':
+        print(square(float(tokens[0])))
+    elif tokens[1] =='cube':
+        print(cube(float(tokens[0])))
+    elif tokens[1] == 'pow':
+        print(power(float(tokens[0]), float(tokens[2])))
+    elif tokens[1] == 'mod':
+        print(mod(float(tokens[0]), float(tokens[2])))
